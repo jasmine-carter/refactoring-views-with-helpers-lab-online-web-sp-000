8a8ce78b8915051c43a24845c2d6ca766563bb5d
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-15 23:53:20 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,9 +8,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    a = Artist.find_or_create_by(name: name)
-    if a != ""
-      self.artist = Artist.find_or_create_by(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
 end
