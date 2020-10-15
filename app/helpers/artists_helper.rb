e9dc7db3f01656edb2ac3792479c98263a3c555c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-15 23:39:03 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
     if song.artist == nil
       link_to "Add Artist", edit_song_path(song)
     else
-      link_to @song.artist_name, @song.artist
+      link_to song.artist_name, song.artist
     end
   end
 
